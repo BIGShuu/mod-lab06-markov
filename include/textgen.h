9a37713b19d1f07d1c411de54603baa4ee23eca4
BIGShuu <renttrich@gmail.com>
2026-06-28 00:52:02 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,6 +1,6 @@
-#ifndef TEXTGEN_H
-#define TEXTGEN_H
-
+// Copyright 2026 UNN-IASR
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
 #include <string>
 #include <deque>
 #include <map>
@@ -12,17 +12,19 @@ typedef std::deque<std::string> prefix;
 typedef std::map<prefix, std::vector<std::string>> statetab;
 
 class TextGenerator {
-private:
-    statetab states;
-    std::mt19937 rng;
-    prefix build_prefix(const prefix& p, const std::string& word) const;    
-public:
-    TextGenerator(unsigned seed = 42);
-    void read_file(const std::string& filename);
-    std::string generate(int max_words = MAXGEN);
-    const statetab& get_states() const { return states; }
-    prefix make_prefix(const std::vector<std::string>& words) const;
-    std::string choose_suffix(const prefix& p) const;
+ private:
+  statetab states;
+  mutable std::mt19937 rng;
+  prefix build_prefix(const prefix& p,
+                      const std::string& word) const;
+ public:
+  explicit TextGenerator(unsigned seed = 42);
+  void read_file(const std::string& filename);
+  std::string generate(int max_words = MAXGEN);
+  const statetab& get_states() const { return states; }
+  prefix make_prefix(const std::vector<std::string>& words)
+      const;
+  std::string choose_suffix(const prefix& p) const;
 };
 std::vector<std::string> split_words(const std::string& text);
-#endif
+#endif  // INCLUDE_TEXTGEN_H_
